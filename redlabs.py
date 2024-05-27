c5f86300ed6c8971d6b08239e1efeda89605b7b9
RFS <7137874+rfs85@users.noreply.github.com>
2024-05-27 02:21:16 +0100

    rfs

--- redlabs.py
+++ redlabs.py
@@ -1,6 +1,7 @@
 import argparse
 
-def install(config_file):
+
+def install_local(config_file):
   print(f"Installing software based on config: {config_file}")
 
 def configure(config_file):
@@ -10,13 +11,20 @@ def delete():
   print("Deleting software and configuration")
 
 def main():
-  parser = argparse.ArgumentParser(description="Software installer and configurator")
-  parser.add_argument("action", choices=["install", "config", "delete"], help="Action to perform (install, config, or delete)")
-  parser.add_argument("-i", "--install", dest="config_file", required=True, help="Configure the Labs")
+  parser = argparse.ArgumentParser(
+                    prog='Red Labs',
+                    description='Red Labs Cloud installer',
+                    epilog='Martian Defense Team')
+  parser.add_argument("action", choices=["install", "delete"], help="What we can do - for now...")
+  parser.add_argument("local", choices=["local", "Any Linux", "ProxMox","VmWare"], help="Deploy to Anywhere")
+  parser.add_argument("cloud", choices=["digitalOcean", "AWS", "Azure","GCP"], help="Deploy to any Cloud")
+  parser.add_argument("-i", "--install", required=True, help="Install the Labs")
+  parser.add_argument("-c", "--config", dest="config_file", required=False, help="Configure the Labs")
+  parser.add_argument("-d", "--delete", required=False, help="Delete the Labs")
   args = parser.parse_args()
 
-  if args.action == "install":
-    install(args.config_file)
+  if args.action == "install" and args.local == 'local':
+    install_local(args.config_file)
   elif args.action == "config":
     configure(args.config_file)
   elif args.action == "delete":
