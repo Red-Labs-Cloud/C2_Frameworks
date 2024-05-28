8cd58ee9088a88bddafc251ba5458dd54a61d283
RFS <7137874+rfs85@users.noreply.github.com>
2024-05-28 05:09:02 +0100

    rfs0

--- redlabs.py
+++ redlabs.py
@@ -1,36 +1,47 @@
 import argparse
+import os
 
-
-def install_local(config_file):
+def c2_local(config_file):
   print(f"Installing software based on config: {config_file}")
+  os.system("ansible-playbook C2s/CobaltStrike/cs-install.yml -i ./local_host")
 
-def configure(config_file):
-  print(f"Configuring software based on config: {config_file}")
+def install_cloud(config_file):
+  os.system("ansible-playbook C2s/CobaltStrike/cs-install.yml -i ./cloud_hosts")
 
 def delete():
   print("Deleting software and configuration")
 
 def main():
-  parser = argparse.ArgumentParser(
+  install = argparse.ArgumentParser(
                     prog='Red Labs',
                     description='Red Labs Cloud installer',
                     epilog='Martian Defense Team')
-  parser.add_argument("action", choices=["install", "delete"], help="What we can do - for now...")
-  parser.add_argument("local", choices=["local", "Any Linux", "ProxMox","VmWare"], help="Deploy to Anywhere")
-  parser.add_argument("cloud", choices=["digitalOcean", "AWS", "Azure","GCP"], help="Deploy to any Cloud")
-  parser.add_argument("-i", "--install", required=True, help="Install the Labs")
-  parser.add_argument("-c", "--config", dest="config_file", required=False, help="Configure the Labs")
-  parser.add_argument("-d", "--delete", required=False, help="Delete the Labs")
-  args = parser.parse_args()
+  install.add_argument("-i", "--install",  action='append',required=False, help="Configure the Labs")
+  install.add_argument("-c", "--config", dest="config_file", required=False, help="Configure the Labs")
+  install.add_argument("-d", "--delete", required=False, help="Delete the Labs")
+  
+  deploy = install.add_argument_group('Deploy')
+  deploy.add_argument('--lh',"--localhost",  action='append',help='Localhost')
+  deploy.add_argument('--dg', help='Digital Ocean Cloud')
+  deploy.add_argument('--aws', help='AWS Cloud')
+  deploy.add_argument('--azr', help='Azure Cloud')
 
-  if args.action == "install" and args.local == 'local':
-    install_local(args.config_file)
-  elif args.action == "config":
-    configure(args.config_file)
-  elif args.action == "delete":
+  service = install.add_argument_group('Services')
+  service.add_argument('--rl', help='Relay')
+  service.add_argument('--c2', help='C2')
+
+
+  #install.print_help()
+  args = install.parse_args()
+
+  if args.install and args.lh:
+    c2_local(args.config_file)
+  elif args.install == "cloud" and args.lh == "deploys":
+    install_cloud(args.config_file)
+  elif args.install == "delete":
     delete()
   else:
-    print(f"Invalid action: {args.action}")
+    print(f"Invalid action: {args.install}")
 
 if __name__ == "__main__":
   main()
